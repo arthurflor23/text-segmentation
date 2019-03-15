229a10990e691b297c5d30d100d478763cfe1408
Arthur Flôr <arthurflor23@gmail.com>
2019-03-15 20:34:32 -0300

    up final files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# handwritten-text-recognition
+# Handwritten Text Segmentation
+
+Project in Python with the handwritten text segmentation module in CPP.
+
+## Techniques
+
+* Document Scanner
+* Binarization with illumination compensation
+* Line Segmentation with deslanting
+* Word Segmentation
--- src/main.py
+++ src/main.py
@@ -8,43 +8,21 @@ pn_OUT = os.path.join("..", "out")
 
 def main():
 	parser = argparse.ArgumentParser()
-	### preprocess actions ###
 	parser.add_argument("-c", "--compile", help="compile cpp file preprocess", action="store_true", default=False)
 	parser.add_argument("-p", "--preprocess", help="execute cpp file preprocess", action="store_true", default=False)
-
-	### data actions ###
 	parser.add_argument("-i", "--image", help="use specific image in data path", type=str, default=None)
-	parser.add_argument("-d", "--dataset", help="use specific dataset in data path", type=str, default=None)
-
-	### model actions ###
-	parser.add_argument("-t", "--train", help="train model", action="store_true", default=False)
-	parser.add_argument("-e", "--evaluate", help="evaluate model", action="store_true", default=False)
 	args = parser.parse_args()
 
-
-	### data functions ###
-	if args.dataset:
-		images = sorted(glob(os.path.join(pn_SRC, "*.png"))) ## CHANGE
-	elif args.image:
+	if args.image:
 		images = sorted(glob(os.path.join(pn_SRC, args.image)))
 	else:
 		images = sorted(glob(os.path.join(pn_SRC, "*.png")))
 
-
-	### preprocess functions ###
 	if args.compile:
 		imgproc.compile()
 
 	if args.preprocess:
 		imgproc.execute(images, pn_OUT)
-
-
-	### model functions ###
-	if args.dataset and args.train:
-		print("train model")
-
-	if args.dataset and args.evaluate:
-		print("evaluate model")
 
 
 if __name__ == '__main__':
